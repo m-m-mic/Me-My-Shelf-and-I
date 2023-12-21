e25327c8b8878c286dc21c9ce7ddc946904e3f2a
m-m-mic <michael.j.buchholz@t-online.de>
2023-12-21 09:40:55 +0100

    Feat: Add more sections to landing page

--- src/app/features/landing-page/landing-page.component.ts
+++ src/app/features/landing-page/landing-page.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   standalone: true,
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss'],
-  imports: [RouterLink],
+  imports: [RouterLink, ButtonModule],
 })
-export class LandingPageComponent {}
+export class LandingPageComponent {
+  router = inject(Router);
+
+  toSignUp() {
+    this.router.navigate(['/sign-up']);
+  }
+
+  toSignIn() {
+    this.router.navigate(['/sign-in']);
+  }
+}
