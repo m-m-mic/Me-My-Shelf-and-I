fe7ecc203b8c1ef87a1636e20b14bb1c72362208
Michael <michael.j.buchholz@t-online.de>
2023-12-22 12:25:30 +0100

    feat: add images, improve styling

--- src/app/features/landing-page/landing-page.component.ts
+++ src/app/features/landing-page/landing-page.component.ts
@@ -1,13 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss'],
-  imports: [RouterLink, ButtonModule],
+  imports: [RouterLink, ButtonModule, NgOptimizedImage],
 })
 export class LandingPageComponent {
   router = inject(Router);
